c224c3002d6009f7dbb75c0023f51935f7e295a1
Muammer Akca <muammerakca10@gmail.com>
2022-09-27 20:37:08 +0300

    startScanning method changed because of deprecated (CLBeaconRegion)

--- MeasureLocation/ViewController.swift
+++ MeasureLocation/ViewController.swift
@@ -22,17 +22,8 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     }
     
     
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        let status = CLAuthorizationStatus(rawValue: Int32())
-        if status == CLAuthorizationStatus.authorizedAlways {
-            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self){
-                if CLLocationManager.isRangingAvailable(){
-                    //do stuff
-                }
-            }
-        }
-    }
     
+    //Deprecated version
     /*
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedAlways {
@@ -45,6 +36,40 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     }
      */
 
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        let status = CLAuthorizationStatus(rawValue: Int32())
+        if status == CLAuthorizationStatus.authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self){
+                if CLLocationManager.isRangingAvailable(){
+                    //do stuff
+                }
+            }
+        }
+    }
+    //Deprecated version
+    func startScanning() {
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+
+        locationManager?.startMonitoring(for: beaconRegion)
+        locationManager?.startRangingBeacons(in: beaconRegion)
+    }
+    
+    //Not deprecated version trying
+    /*
+    func startScanning() {
+        
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        //let beaconRegion = CLBeaconRegion(proximityUUID: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        let beaconRegion = CLBeaconRegion(uuid: uuid, major: 123, minor: 456, identifier: "myBeacon")
+        let beaconIdentity = CLBeaconIdentityConstraint(uuid: uuid, major: 123, minor: 456)
+        locationManager?.startMonitoring(for: beaconRegion)
+        //locationManager?.startRangingBeacons(in: beaconRegion)
+        locationManager?.startRangingBeacons(satisfying: beaconIdentity)
+    }
+     */
+    
 
 }
 
