af43214361aafb2dde0fcf420fae95ab41483b08
Muammer Akca <muammerakca10@gmail.com>
2022-09-27 19:36:01 +0300

    Added CLLocationManager functions and controls

--- MeasureLocation/ViewController.swift
+++ MeasureLocation/ViewController.swift
@@ -4,14 +4,30 @@
 //
 //  Created by MAC on 26.09.2022.
 //
-
+import CoreLocation
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate {
 
+    @IBOutlet var distanceReading: UILabel!
+    var locationManager : CLLocationManager?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestAlwaysAuthorization()
+        
+        view.backgroundColor = .gray
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self){
+                if CLLocationManager.isRangingAvailable(){
+                    
+                }
+            }
+        }
     }
 
 
